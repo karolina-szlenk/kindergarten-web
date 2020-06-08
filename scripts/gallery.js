110aa49490a971d917fa2cf1c6732151c8bde4a5
Karolina <karolina.szlenk@gmail.com>
2020-06-08 22:55:13 +0200

    code optimized

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,9 +1,8 @@
 const gallery = [
   {
     name: "Urodzinki Lenki",
-    img:
-      "https://www.taniec.walbrzych.pl/assets/spaw2/uploads/images/kopalnia-soli-wieliczka-kopalnia-wiedzy-jak-zorganizowa-urodziny-firmowej2-700-465-09-01-2018.jpg",
     pictures: [
+      "https://www.taniec.walbrzych.pl/assets/spaw2/uploads/images/kopalnia-soli-wieliczka-kopalnia-wiedzy-jak-zorganizowa-urodziny-firmowej2-700-465-09-01-2018.jpg",
       "https://www.prawo.pl/gfx/prawopl/_thumbs/pl/wkdokumenty/000/300/576/1/dzieci-00020-thinkstockphotos,oX6D62aapFTLo7bXW5mZ.jpg.webp",
       "https://cdn.galleries.smcloud.net/t/galleries/gf-qJYe-hwyT-todU_zabawy-urodzinowe-664x442-nocrop.jpg",
       "https://www.taniec.walbrzych.pl/assets/spaw2/uploads/images/kopalnia-soli-wieliczka-kopalnia-wiedzy-jak-zorganizowa-urodziny-firmowej2-700-465-09-01-2018.jpg",
@@ -11,9 +10,8 @@ const gallery = [
   },
   {
     name: "Wigilia 2019",
-    img:
-      "https://cdn.pixabay.com/photo/2018/05/16/10/11/christmas-3405368_960_720.jpg",
     pictures: [
+      "https://cdn.pixabay.com/photo/2018/05/16/10/11/christmas-3405368_960_720.jpg",
       "https://www.prawo.pl/gfx/prawopl/_thumbs/pl/wkdokumenty/000/300/576/1/dzieci-00020-thinkstockphotos,oX6D62aapFTLo7bXW5mZ.jpg.webp",
       "https://cdn.galleries.smcloud.net/t/galleries/gf-qJYe-hwyT-todU_zabawy-urodzinowe-664x442-nocrop.jpg",
       "https://www.taniec.walbrzych.pl/assets/spaw2/uploads/images/kopalnia-soli-wieliczka-kopalnia-wiedzy-jak-zorganizowa-urodziny-firmowej2-700-465-09-01-2018.jpg",
@@ -21,9 +19,8 @@ const gallery = [
   },
   {
     name: "Bal karnawałowy",
-    img:
-      "https://image.freepik.com/free-photo/funny-girl-cat-costume-touching-hair_23-2147925802.jpg",
     pictures: [
+      "https://image.freepik.com/free-photo/funny-girl-cat-costume-touching-hair_23-2147925802.jpg",
       "https://www.prawo.pl/gfx/prawopl/_thumbs/pl/wkdokumenty/000/300/576/1/dzieci-00020-thinkstockphotos,oX6D62aapFTLo7bXW5mZ.jpg.webp",
       "https://cdn.galleries.smcloud.net/t/galleries/gf-qJYe-hwyT-todU_zabawy-urodzinowe-664x442-nocrop.jpg",
       "https://www.taniec.walbrzych.pl/assets/spaw2/uploads/images/kopalnia-soli-wieliczka-kopalnia-wiedzy-jak-zorganizowa-urodziny-firmowej2-700-465-09-01-2018.jpg",
@@ -42,27 +39,20 @@ gallery.forEach(function (item) {
   heading.innerText = item.name;
   const picturesContainer = document.createElement("div");
   picturesContainer.classList.add("gallery");
-  const link = document.createElement("a");
-  link.classList.add("image");
-  link.setAttribute("href", item.img);
-  const img = document.createElement("img");
-  img.classList.add("info__image");
-  img.setAttribute("src", item.img);
-
   const pictures = item.pictures;
-  pictures.forEach(function (el) {
-    const link2 = document.createElement("a");
-    link2.setAttribute("href", el);
-    const img2 = document.createElement("img");
-    img2.setAttribute("src", el);
-    link2.classList.add("hide", "image");
-    link2.append(img2);
-    picturesContainer.append(link2);
+  pictures.forEach(function (el, index) {
+    const link = document.createElement("a");
+    link.setAttribute("href", el);
+    const img = document.createElement("img");
+    img.setAttribute("src", el);
+    img.classList.add("info__image");
+    link.classList.add("image");
+    if (index > 0) {
+      link.classList.add("hide");
+    }
+    link.append(img);
+    picturesContainer.append(link);
   });
-
-  picturesContainer.prepend(link);
-
-  link.append(img);
   article.prepend(heading);
   article.append(picturesContainer);
   div.append(article);
