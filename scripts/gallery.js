09c9daba0052f472d81d2017bded398fc0177c72
Karolina <karolina.szlenk@gmail.com>
2020-06-08 12:15:08 +0200

    pop-up in gallery created

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,8 +1,8 @@
 const gallery = [
   {
     name: "Urodzinki Lenki",
-    img: "../img/edu.jpg",
-    href: "urodzinki-lenki.html",
+    img:
+      "https://www.taniec.walbrzych.pl/assets/spaw2/uploads/images/kopalnia-soli-wieliczka-kopalnia-wiedzy-jak-zorganizowa-urodziny-firmowej2-700-465-09-01-2018.jpg",
     pictures: [
       "https://www.prawo.pl/gfx/prawopl/_thumbs/pl/wkdokumenty/000/300/576/1/dzieci-00020-thinkstockphotos,oX6D62aapFTLo7bXW5mZ.jpg.webp",
       "https://cdn.galleries.smcloud.net/t/galleries/gf-qJYe-hwyT-todU_zabawy-urodzinowe-664x442-nocrop.jpg",
@@ -11,8 +11,8 @@ const gallery = [
   },
   {
     name: "Wigilia 2019",
-    img: "../img/meal.jpg",
-    href: "wigilia-2019.html",
+    img:
+      "https://cdn.pixabay.com/photo/2018/05/16/10/11/christmas-3405368_960_720.jpg",
     pictures: [
       "https://www.prawo.pl/gfx/prawopl/_thumbs/pl/wkdokumenty/000/300/576/1/dzieci-00020-thinkstockphotos,oX6D62aapFTLo7bXW5mZ.jpg.webp",
       "https://cdn.galleries.smcloud.net/t/galleries/gf-qJYe-hwyT-todU_zabawy-urodzinowe-664x442-nocrop.jpg",
@@ -21,15 +21,14 @@ const gallery = [
   },
   {
     name: "Bal karnawałowy",
-    img: "../img/meal.jpg",
-    href: "bal-karnawalowy.html",
+    img:
+      "https://image.freepik.com/free-photo/funny-girl-cat-costume-touching-hair_23-2147925802.jpg",
     pictures: [
       "https://www.prawo.pl/gfx/prawopl/_thumbs/pl/wkdokumenty/000/300/576/1/dzieci-00020-thinkstockphotos,oX6D62aapFTLo7bXW5mZ.jpg.webp",
       "https://cdn.galleries.smcloud.net/t/galleries/gf-qJYe-hwyT-todU_zabawy-urodzinowe-664x442-nocrop.jpg",
       "https://www.taniec.walbrzych.pl/assets/spaw2/uploads/images/kopalnia-soli-wieliczka-kopalnia-wiedzy-jak-zorganizowa-urodziny-firmowej2-700-465-09-01-2018.jpg",
     ],
   },
-  
 ];
 
 const mainDescription = document.querySelector(".main__description");
@@ -41,14 +40,42 @@ gallery.forEach(function (item) {
   const heading = document.createElement("h3");
   heading.classList.add("info__title");
   heading.innerText = item.name;
+  const picturesContainer = document.createElement("div");
+  picturesContainer.classList.add("gallery");
   const link = document.createElement("a");
-  link.setAttribute("href", item.href);
+  link.classList.add("image");
+  link.setAttribute("href", item.img);
   const img = document.createElement("img");
   img.classList.add("info__image");
   img.setAttribute("src", item.img);
+
+  const pictures = item.pictures;
+  pictures.forEach(function (el) {
+    const link2 = document.createElement("a");
+    link2.setAttribute("href", el);
+    const img2 = document.createElement("img");
+    img2.setAttribute("src", el);
+    link2.classList.add("hide", "image");
+    link2.append(img2);
+    picturesContainer.append(link2);
+  });
+
+  picturesContainer.prepend(link);
+
   link.append(img);
   article.prepend(heading);
-  article.append(link);
+  article.append(picturesContainer);
   div.append(article);
 });
 mainDescription.append(div);
+
+// the container for each gallery
+$(".gallery").each(function () {
+  $(this).magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
