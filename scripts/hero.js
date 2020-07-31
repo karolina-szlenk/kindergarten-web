2f8d4026cbac2db01079720b23e4e4ed715ac608
Karolina <karolina.szlenk@gmail.com>
2020-07-31 07:19:59 +0200

    content changed

--- scripts/hero.js
+++ scripts/hero.js
@@ -1,1 +1,10 @@
-const slide = new Slider("#slider1");
+const slide = new Slider('#slider1')
+
+const showHeroText = function () {
+  const texts = document.querySelectorAll('.hero__text')
+  texts.forEach((txt) => {
+    txt.classList.add('hide', 'show')
+  })
+}
+
+showHeroText()
