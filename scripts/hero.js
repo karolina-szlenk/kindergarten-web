50bba2542dd032d502c17f6b2ead965ffc8376c1
Karolina <karolina.szlenk@gmail.com>
2020-08-03 18:42:52 +0200

    name of style changed

--- scripts/hero.js
+++ scripts/hero.js
@@ -3,7 +3,7 @@ const slide = new Slider('#slider1')
 const showHeroText = function () {
   const texts = document.querySelectorAll('.hero__text')
   texts.forEach((txt) => {
-    txt.classList.add('hide', 'show')
+    txt.classList.add('hide', 'visible')
   })
 }
 
